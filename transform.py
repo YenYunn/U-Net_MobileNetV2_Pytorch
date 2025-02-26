9316a4e4887ba219e903d683df7d7ae72e1120f2
Yen <48754757+YenYunn@users.noreply.github.com>
2025-02-26 15:39:35 +0800

    Update transform.py
    add augmentation way

--- transform.py
+++ transform.py
@@ -4,15 +4,17 @@ from albumentations.pytorch.transforms import ToTensor
 train_transform = albu.Compose([albu.Resize(320, 320),
                                 albu.HorizontalFlip(p=0.5),
                                 albu.VerticalFlip(p=0.5),
-                                # albu.CenterCrop(width=289, height=289, p=0.1),
-                                # albu.RandomBrightness(p=0.1),
-                                # albu.OneOf([albu.RandomContrast(p=1),
-                                #             albu.HueSaturationValue(p=1)],
-                                #            p=0.5),
-                                # albu.Normalize(mean=MEAN, std=STD),
+                                albu.OneOf([
+                                    albu.RandomBrightness(limit=0.2, p=1),
+                                    albu.RandomContrast(limit=0.2, p=1),
+                                    albu.HueSaturationValue(hue_shift_limit=0, sat_shift_limit=20, val_shift_limit=0,
+                                                            p=1)
+                                ], p=0.5),
+                                albu.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                                 ToTensor()
                                 ])
 
 val_transform = albu.Compose([albu.Resize(320, 320),
+                              albu.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                               ToTensor()
                               ])
