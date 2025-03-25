b33a25a8808c69f46ef6a49aeead2de917b2c261
Yen <48754757+YenYunn@users.noreply.github.com>
2025-03-25 09:16:00 +0800

    Update torch2onnx.py

--- tools/torch2onnx.py
+++ tools/torch2onnx.py
@@ -1,9 +1,7 @@
-import cv2
 import os
-import onnx
 import torch
+import onnx
 import onnxsim
-from torchvision import transforms
 
 
 def load_model(model_path, device):
@@ -15,22 +13,9 @@ def load_model(model_path, device):
     return model
 
 
-def preprocess_image(img_path, img_size=(320, 320)):
-    if not os.path.exists(img_path):
-        raise FileNotFoundError(f"Image file not found: {img_path}")
-
-    image = cv2.imread(img_path)
-    image = cv2.resize(image, img_size)
-
-    transform = transforms.Compose([
-        transforms.ToTensor()
-    ])
-    return transform(image).unsqueeze(0)
-
-
-def export_onnx(model, image_tensor, export_path, input_names, output_names):
+def export_onnx(model, dummy_input, export_path, input_names, output_names):
     torch.onnx.export(
-        model, image_tensor, export_path,
+        model, dummy_input, export_path,
         verbose=True, input_names=input_names, output_names=output_names
     )
     print(f'ONNX model exported to {export_path}')
@@ -45,24 +30,27 @@ def simplify_onnx(onnx_path, simplified_path):
     else:
         print('ONNX simplification failed')
 
+
 def main():
-    model_path = '../model/train1/weights/best.pt'
-    img_path = '../data/images/0000.jpg'
-    export_path = '../model/train1/weights/best.onnx'
-    simplified_path = '../model/train1/weights/best_simplified.onnx'
+    model_path = '../runs/train8/weights/best.pt'
     img_size = (320, 320)
+    input_names = ['input_2']
+    output_names = ['Identity']
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     try:
         model = load_model(model_path, device)
-        image_tensor = preprocess_image(img_path, img_size).to(device)
 
-        input_names = ['input_2']
-        output_names = ['Identity']
+        dummy_input = torch.randn(1, 3, img_size[1], img_size[0]).to(device)
 
-        export_onnx(model, image_tensor, export_path, input_names, output_names)
+        model_dir = os.path.dirname(model_path)
+        export_path = os.path.join(model_dir, 'best.onnx')
+        simplified_path = os.path.join(model_dir, 'best_simplified.onnx')
+
+        export_onnx(model, dummy_input, export_path, input_names, output_names)
         simplify_onnx(export_path, simplified_path)
+
     except Exception as e:
         print(f'Error: {e}')
 
